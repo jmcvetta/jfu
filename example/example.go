e1649062f2bcfe96e17bc3ac8f73dad228c4d65f
Jason McVetta <jason.mcvetta@gmail.com>
2012-07-04 12:34:13 -0700

    Vestigal example app

---
+++ example/example.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/jmcvetta/jfu"
+	"labix.org/v2/mgo"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	//
+	path := os.Getenv("PWD")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	url := "0.0.0.0:" + port
+	//
+	conn, err := mgo.Dial("localhost")
+	if err != nil {
+		log.Panic(err)
+	}
+	db := conn.DB("test_foobar")
+	gfs := db.GridFS("test_foobar")
+	store := jfu.NewMongoStore(gfs)
+	uh := jfu.UploadHandler{
+		Store: store,
+	}
+	log.Println(uh)
+	//
+	http.Handle("/", http.FileServer(http.Dir(path)))
+	log.Println("Starting webserver on " + url + "...")
+	http.ListenAndServe(url, nil)
+}
